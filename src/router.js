3002f9c79bb9f0d452825a7836c843ce9784b81e
Ondřej Kotas <ok@krtkovo.eu>
2019-09-25 21:11:47 +0200

    client info datetime format

--- src/router.js
+++ src/router.js
@@ -353,9 +353,9 @@ function saveClientLog(req) {
     let date = ("0" + date_ob.getDate()).slice(-2);
     let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
     let year = date_ob.getFullYear();
-    let hours = date_ob.getHours();
-    let minutes = date_ob.getMinutes();
-    let seconds = date_ob.getSeconds();
+    let hours = ("0" + date_ob.getHours()).slice(-2);
+    let minutes = ("0" + date_ob.getMinutes()).slice(-2);
+    let seconds = ("0" + date_ob.getSeconds()).slice(-2);
     
     db.rdb.table("clientLog").insert({
         "DateTime" : year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds,
